0f19fa6d129851c763dbe813d6d797a5badc09fe
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-12-02 09:32:57 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+import { LoginModule, LoginRoutingModule, CadastroPjModule } from './autenticacao';
+import { CadastroPjRoutingModule } from './autenticacao/cadastro-pj/cadastro-pj-routing.module';
+
+/**
+ * Módulo de routing raiz deve ser sempre o último na declaração no import
+ * 
+ */
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LoginModule,
+    CadastroPjModule,
+    
+    LoginRoutingModule,
+    CadastroPjRoutingModule,
     AppRoutingModule
   ],
   providers: [],
